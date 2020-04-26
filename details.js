8511834ca9956d64ea651383a69f1cf6d8933ab5
anele-jay <60442392+anele-jay@users.noreply.github.com>
2020-04-26 23:24:55 +0200

    Update details.js

--- details.js
+++ details.js
@@ -1,1 +1,29 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
 
+const name = params.get("name");
+
+const url = "https://www.amiiboapi.com/api/amiibo/name/" + name;
+
+async function getGameBYName() {
+    const heading = document.querySelector(".btn");
+
+    try {
+        const response = await fetch(url);
+        const details = await response.json();
+
+        console.log("details", details);
+
+
+        heading.innerHTML = details.name;
+
+    } catch (error) {
+        heading.innerHTML = "An error occured" ;
+        console.log(error);
+    } finally {
+        const loading = document.querySelector(".loading");
+        loading.style.display = "none";
+    }
+}
+
+getGameBYName();
