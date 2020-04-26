a36daaeec8345cb5ef80e92927757d95c3156806
anele-jay <60442392+anele-jay@users.noreply.github.com>
2020-04-26 23:22:14 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+//first try fetching the url details
+fetch("https://www.amiiboapi.com/api/amiibo/")
+.then(function(response){
+	return response.json();
+})
+.then(function(json){
+	viewResults(json);
+})
+.catch(function(error) {
+	console.log(error);
+});
+
+function viewResults(json) {
+    const content = json.amiibo;
+
+    let html = "";
+
+    for (let i = 0; i < content.length; i++) {
+        let activeClass = "";
+
+        if (i === 0) {
+            activeClass = "active";
+        }
+
+		html += `<div class="view-Item ${activeClass}">
+		            <p> ${content[i].name} </p>
+                    <img src="${content[i].image}" class="d-block w-100">
+                </div>`;
+    }
+
+    const carouselInner = document.querySelector("#viewRes");
+    carouselInner.innerHTML = html;
+}
